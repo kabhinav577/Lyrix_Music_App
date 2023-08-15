32f1a814b42a9b16a2a0cb4ceacc4bb4de8ea421
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-08-15 19:40:36 +0530

    First Commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,47 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// const options = {
+//     method: 'GET',
+//     headers: {
+//       'X-RapidAPI-Key': '8a1432b603mshffdc0801d6c7fc6p1d4e3ajsnb4387bbd53e8',
+//       'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+//     }
+//   };
+
+//   fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "8a1432b603mshffdc0801d6c7fc6p1d4e3ajsnb4387bbd53e8"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `/tracks/related?track_id=${songid}`,
+    }),
+    getArtistDetails: builder.query({ query: (artistsId) => `/artists/details?artist_id=${artistsId}`}),
+    getSongsByCountry: builder.query({ query : (countryCode) =>`/charts/country?country_code=${countryCode}`}),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
+} = shazamCoreApi;
